630bfec8a11ab762369cc1e47058dda33ee80bd4
Vicki Hamm <vicki.cello@gmail.com>
2018-06-21 14:39:07 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,7 +8,7 @@ def hello_t(array)
 end
 
 # call your method here!
-hello_t()["Tim", "Tom", "Jim"]) do |name|
+hello_t()["Tim", "Tom", "Jim"]).each do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
